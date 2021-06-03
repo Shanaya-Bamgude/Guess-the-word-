4c39ed204de4478804f3aff3894bccf7122d7559
Shanaya-Bamgude <83908410+Shanaya-Bamgude@users.noreply.github.com>
2021-06-03 12:39:48 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,15 @@
+
+
+function getScore()
+{
+
+    score=localStorage.getItem("score");
+    document.getElementById("update").innerHTML="<h1>score:" + score + "</h1>";
+
+}
+
+
+function back() {
+    window.location = "activity_1.html";
+}
+
